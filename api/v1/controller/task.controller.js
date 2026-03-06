e8744e93064b6059204da07b6da43728e0edf7c9
Ryu <nguyenductruong200604@gmail.com>
2026-03-06 10:33:31 +0700

    Lấy ra danh sách theo đúng user

--- api/v1/controller/task.controller.js
+++ api/v1/controller/task.controller.js
@@ -4,6 +4,10 @@ const searchHepler = require("../../../helpers/search")
 // [GET] api/v1/tasks
 module.exports.index = async (req, res) => {
     const find = {
+        $or: [
+            { createdBy: req.user.id },
+            { listUser: req.user.id }
+        ],
         deleted: false
     }
     if (req.query.status) {
